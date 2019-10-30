c3adcb88abfbfd9e7d5a02e608e5c3a7a050005a
Aaron Chan <aaron.y.chan64@gmail.com>
2019-10-30 09:27:00 -0600

    Revert "added get method for availabilies"

--- server/routes.py
+++ server/routes.py
@@ -4,7 +4,7 @@ from server.api.login import Login
 from server.api.update_user import UpdateUser
 from server.api.get_languages import GetLanguages
 from server.api.search_timezone import SearchTimezone
-from server.api.availabilities import AddAvailability
+from server.api.add_availability import AddAvailability
 from server.api.search_availabilities import SearchAvailabilities
 from server.api.accept_request import AcceptRequest
 from server.api.request import Request
