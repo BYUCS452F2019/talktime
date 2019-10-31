0b422932c375dc94cba6c5abd64630055fcc1eb3
Brett Fisher <bf6038@gmail.com>
2019-10-31 17:47:19 -0600

    Added timezone insertion

---
+++ server/models/Timezones.py
@@ -0,0 +1,15 @@
+from flask import request
+from flask_restplus import Resource, fields
+from server.app import api, db
+from server.models.Availabilities import Availabilities
+from server.api import token_required
+
+class Timezones(db.Model):
+  id = db.Column(db.Integer, primary_key=True, nullable=False)
+  name = db.Column(db.String(64), nullable=False)
+  t_offset = db.Column(db.Integer, nullable=False)
+
+  def __init__(self, id, name, offset):
+    self.id = id
+    self.name = name
+    self.t_offset = offset
