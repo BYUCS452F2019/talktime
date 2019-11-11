d154835d3c27165bf059affdb686f646f60344b6
Brett Fisher <bf6038@gmail.com>
2019-11-10 19:33:12 -0700

    Added back get opening request

--- server/routes.py
+++ server/routes.py
@@ -10,3 +10,4 @@ from server.api.search_availabilities import SearchAvailabilities
 from server.api.accept_request import AcceptRequest
 from server.api.get_user import GetUser
 from server.api.chat_request import Request
+from server.api.get_opening_request import GetOpeningRequest
