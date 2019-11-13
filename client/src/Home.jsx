7f60eee0efa426c0817d00708560f2fb76e8007f
Aaron Chan <aaron.y.chan64@gmail.com>
2019-11-12 17:42:47 -0700

    reorg and make room for dashboard

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,14 +3,21 @@ import HomeAppBar from './HomeAppBar.jsx';
 import Settings from './Settings.jsx';
 import Search from './Search.jsx';
 import Availability from './Availability.jsx';
+import Dashboard from './Dashboard.jsx';
 import { Switch, Route } from 'react-router-dom';
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
   render () {
     return (
       <div>
         <HomeAppBar />
         <Switch>
+          <Route exact path="/home" component={Dashboard} />
           <Route exact path="/home/search" component={Search} />
           <Route exact path="/home/settings" component={Settings} />
           <Route exact path="/home/availability" component={Availability} />
--- client/src/MeetingTabs.jsx
+++ client/src/MeetingTabs.jsx
@@ -8,9 +8,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 function TabContainer(props) {
   return (
@@ -33,8 +32,6 @@ const useStyles = makeStyles(theme => ({
 
 export default function MeetingTabs() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-  const [requests, setRequests] = React.useState([])
   const [open_requests, setOpenRequests] = React.useState([])
   const [upcoming_meetings, setUpcomingMeetings] = React.useState([])
 
@@ -52,12 +49,14 @@ export default function MeetingTabs() {
         console.log("got reslt: " + JSON.stringify(result))
 
         let sample = [{
+          "user_name": "bob",
           "other_user_id": 0,
           "from_time": 0,
           "to_time": 0,
           "req_accepted": true,
           "req_confirmed": true
         }, {
+          "user_name": "jane",
           "other_user_id": 1,
           "from_time": 0,
           "to_time": 0,
@@ -69,10 +68,6 @@ export default function MeetingTabs() {
         setUpcomingMeetings(sample.filter(req => req.req_confirmed && req.req_accepted))
       })
   }, []);
-
-  function handleChange(event, newValue) {
-    setValue(newValue);
-  }
 
   function handleRequest(req, will_accept) {
     console.log(JSON.stringify(req))
@@ -100,44 +95,28 @@ export default function MeetingTabs() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} variant="fullWidth">
-          <Tab label="Upcoming Meetings" />
-          <Tab label="Open Requests" />
-        </Tabs>
-      </AppBar>
-      {value === 0 &&
-       <TabContainer>
-         <List component="nav">
-           {
-           // TODO do we need the exact day/time, username
-             open_requests.map(req =>
-                <ListItem>
-                  <ListItemText primary={req.other_user_id} />
-                </ListItem>)
-           }
-        </List>
-       </TabContainer>}
-      {value === 1 &&
-       <TabContainer>
-         <List component="nav">
-           {
-           // TODO do we need exact day/time, username
-             upcoming_meetings.map(req =>
-                <ListItem>
-                  <ListItemText primary={req.other_user_id} />
-                  <ButtonGroup size="small" variant="contained" color="primary">
-                    <Button variant="contained"
-                            color="primary"
-                            onClick={() => handleRequest(req, true)}>Accept</Button>
-                    <Button variant="contained"
-                            color="secondary"
-                            onClick={() => handleRequest(req, false)}>Reject</Button>
-                  </ButtonGroup>
-                </ListItem>)
-           }
-        </List>
-       </TabContainer>
-      }
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Open Requests
+          </Typography>
+        </Toolbar>
+    </AppBar>
+        <List component="nav">
+          {
+            open_requests.map(req =>
+              <ListItem>
+                <ListItemText primary={req.user_name} />
+                <ButtonGroup size="small" variant="contained" color="primary">
+                  <Button variant="contained"
+                          color="primary"
+                          onClick={() => handleRequest(req, true)}>Accept</Button>
+                  <Button variant="contained"
+                          color="secondary"
+                          onClick={() => handleRequest(req, false)}>Reject</Button>
+                </ButtonGroup>
+              </ListItem>)
+          }
+      </List>
     </div>
   );
 }
