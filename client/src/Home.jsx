791a1ef89bfd297cfd960973d32e3451fb3663d9
Brett Fisher <bf6038@gmail.com>
2019-11-21 18:18:12 -0700

    Fixed app bar

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,23 +1,78 @@
-import React, { Component } from 'react';
-import HomeAppBar from './HomeAppBar.jsx';
-import Settings from './Settings.jsx';
-import Search from './Search.jsx';
-import Availability from './Availability.jsx';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import HomeAppBar from "./HomeAppBar.jsx";
+import Settings from "./Settings.jsx";
+import Search from "./Search.jsx";
+import Availability from "./Availability.jsx";
+import { Switch, Route } from "react-router-dom";
 
-class Home extends Component {
-  render () {
-    return (
-      <div>
-        <HomeAppBar />
-        <Switch>
-          <Route exact path="/home/search" component={Search} />
-          <Route exact path="/home/settings" component={Settings} />
-          <Route exact path="/home/availability" component={Availability} />
-        </Switch>
-      </div>
-    )
-  }
-}
+// class Home extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { notifications: null };
+//   }
+
+//   componentDidMount() {
+//     fetch("/api/notifications", {
+//       method: "get",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//         Authorization: "bearer: " + localStorage.getItem("auth")
+//       }
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         this.setState({ notifications: data });
+//       });
+//   }
+//   render() {
+//     // let notifications = "hi";
+//     // if (this.state) {
+//     //   notifications = this.state.notifications;
+//     //   console.log(notifications);
+//     // }
+//     return (
+//       <div>
+//         <HomeAppBar notifications={this.state.notifications} />
+//         <Switch>
+//           <Route exact path="/home/search" component={Search} />
+//           <Route exact path="/home/settings" component={Settings} />
+//           <Route exact path="/home/availability" component={Availability} />
+//         </Switch>
+//       </div>
+//     );
+//   }
+// }
+
+const Home = () => {
+  const [notifications, setNotifications] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/notifications", {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "bearer: " + localStorage.getItem("auth")
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("returned");
+        setNotifications(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <HomeAppBar notifications={notifications} />
+      <Switch>
+        <Route exact path="/home/search" component={Search} />
+        <Route exact path="/home/settings" component={Settings} />
+        <Route exact path="/home/availability" component={Availability} />
+      </Switch>
+    </div>
+  );
+};
 
 export default Home;
--- server/api/notifications.py
+++ server/api/notifications.py
@@ -1,7 +1,7 @@
 from server.app import mdb
 from flask import request
 from flask_restplus import Resource, fields
-from server.api import token_required
+from server.api import token_required, verify_request
 from server.app import api, db
 from server.models.Requests import Requests
 from server.models.Users import Users
@@ -10,6 +10,13 @@ from bson import ObjectId
 
 NS = api.namespace('notifications',
                    description='View notifications')
+
+READ_PATCH_FORM = {
+    'id': fields.String  # ID of notification to mark as read
+}
+
+READ_PATCH_MODEL = api.model('Read notification model', READ_PATCH_FORM)
+
 
 def mongo_encode(doc):
   enc_doc = {}
@@ -20,6 +27,7 @@ def mongo_encode(doc):
       enc_doc[key] = doc[key]
   return enc_doc
 
+
 @NS.route('')
 class Notifications(Resource):
   @token_required
@@ -29,3 +37,25 @@ class Notifications(Resource):
     for notification in notifications.find({'user_id': user.id}):
       nots.append(mongo_encode(notification))
     return nots
+
+  @NS.expect(READ_PATCH_MODEL)
+  @token_required
+  def patch(self, user):
+    data = request.get_json()
+    if not data:
+      data = request.form
+
+    verify_request(['id'], data)
+    notifications = mdb.notifications
+    query = {'_id': ObjectId(data['id'])}
+    new_values = {'$set': {'read': True}}
+
+    update_notification = notifications.find_one(query)
+
+    if not update_notification:
+      api.abort(400, 'Notification with that id does not exist')
+    if update_notification['user_id'] != user.id:
+      api.abort(400, 'You are not authorized to update this notification')
+
+    notifications.update_one(query, new_values)
+    return "Success"
